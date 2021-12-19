463479a31dc25b00bbfeb11a31b0e6a4b2d893d5
Ben Hur Faria <benhurfaria3@gmail.com>
2021-12-18 21:16:27 -0300

    ajuste

--- src/database.ts
+++ src/database.ts
@@ -9,19 +9,15 @@ if (
 
 export default async function connect() {
   const connectionManager = await getConnectionManager();
-
   const connection = connectionManager.create({
     name: 'default',
     type: 'postgres',
     url: process.env.DATABASE_URL,
     entities: [
-      `${
-        process.env.NODE_ENV === 'production' ? 'dist/src' : 'src'
-      }/entities/*.*`,
+      `${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`,
     ],
     ssl: process.env.NODE_ENV === 'production',
   });
-
   await connection.connect();
   return connection;
 }
