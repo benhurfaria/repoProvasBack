634c24ffdac30ff2d0411e809e7c719ca1d1ad8c
Ben Hur Faria <benhurfaria3@gmail.com>
2021-12-18 21:12:32 -0300

    ajuste

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
-import "./setup";
+import './setup';
 
-import express from "express";
-import cors from "cors";
-import "reflect-metadata";
+import express from 'express';
+import cors from 'cors';
+import 'reflect-metadata';
 
-import connectDatabase from "./database";
+import connectDatabase from './database';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('test');
+  res.send('test');
 });
 
-
-export async function init () {
-    await connectDatabase();
+export async function init() {
+  await connectDatabase();
 }
-  
+
 export default app;
