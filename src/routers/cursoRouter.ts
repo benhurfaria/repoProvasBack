0c74b2574ce302b9e754f822c8d9059b1b9d7686
Matheus Galdino <matheus@respondeai.com.br>
2022-01-04 12:15:03 -0300

    preparando o review

--- src/routers/cursoRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as cursoController from '../controllers/cursoController';
-
-const router = Router();
-
-router.get("/disciplina", cursoController.getCourse);
-router.post("/curso", cursoController.getCourseProf);
-
-export default router;
--- src/routers/professorRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as professorController from '../controllers/professorController';
-
-const router = Router();
-
-router.get('/professores', professorController.getTeachers);
-router.post('/cursoprofessor', professorController.getCurso);
-
-export default router;
